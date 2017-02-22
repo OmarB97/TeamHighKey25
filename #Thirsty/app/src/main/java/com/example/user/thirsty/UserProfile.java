b6ad46d5967608c9b955bee27250432b3ff6387c
Dennis Eddington <Dennis Eddington>
2017-02-22 13:43:29 -0500

    Javadoc added

--- #Thirsty/app/src/main/java/com/example/user/thirsty/UserProfile.java
+++ #Thirsty/app/src/main/java/com/example/user/thirsty/UserProfile.java
@@ -8,10 +8,39 @@ package com.example.user.thirsty;
 
 public abstract class UserProfile {
 
+    /**
+     * Returns the password of the UserProfile
+     *
+     * @return the password of the object
+     */
     abstract String getPassword();
+
+    /**
+     * Returns the email of the UserProfile
+     *
+     * @return the email of the object
+     */
     abstract String getEmail();
+
+    /**
+     * Returns the user type of the UserProfile
+     *
+     * @return the user type of the object
+     */
     abstract String getUserType();
+
+    /**
+     * Sets the password of the UserProfile
+     *
+     * @param password the password to be set to the object
+     */
     abstract void setPassword(String password);
+
+    /**
+     * Sets the email of the UserProfile
+     *
+     * @param email the email to be set to the object
+     */
     abstract void setEmail(String email);
 
 }
