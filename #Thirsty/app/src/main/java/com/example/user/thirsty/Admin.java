ef3b45b96e64959adb4a3208faab254375d5a7d6
Dennis Eddington <Dennis Eddington>
2017-02-22 12:32:43 -0500

    Javadoc added

--- #Thirsty/app/src/main/java/com/example/user/thirsty/Admin.java
+++ #Thirsty/app/src/main/java/com/example/user/thirsty/Admin.java
@@ -9,32 +9,62 @@ public class Admin extends UserProfile {
     private String email;
     private String userType;
 
+    /**
+     *
+     * @param email
+     * @param password
+     * @param userType
+     */
     public Admin(String email, String password, String userType) {
         this.password = password;
         this.email = email;
         this.userType = userType;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getUserType() {
         return userType;
     }
 
+    /**
+     *
+     * @param password
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     *
+     * @param email
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * 
+     * @param userType
+     */
     public void setUserType(String userType) {
         this.userType = userType;
     }
