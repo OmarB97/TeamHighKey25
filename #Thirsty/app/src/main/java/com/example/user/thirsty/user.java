f4b3d4c97b7cd52737a4615f4330c3aa1d0fe420
Dennis Eddington <Dennis Eddington>
2017-02-22 13:06:37 -0500

    Javadoc added

--- #Thirsty/app/src/main/java/com/example/user/thirsty/user.java
+++ #Thirsty/app/src/main/java/com/example/user/thirsty/user.java
@@ -8,33 +8,70 @@ public class User extends UserProfile{
     private String password;
     private String email;
     private String userType;
-
+    /**
+     *Constructor for an user object. Takes in an email, a password, and a user type and assigns
+     * them to the object
+     *
+     * @param email the email for the User object
+     * @param password the password for the User object
+     * @param userType the userType for the User object
+     */
     public User(String email, String password, String userType) {
         this.password = password;
         this.email = email;
         this.userType = userType;
     }
 
+    /**
+     *Returns the password
+     *
+     * @return the string containing the password of the User object
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     *Return the email
+     *
+     * @return the string containing the email of the User object
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *Returns the user type
+     *
+     * @return the string containing the user type of the User object
+     */
     public String getUserType() {
         return userType;
     }
 
+    /**
+     *Sets the password
+     *
+     * @param password the password to be assigned to an User object
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     * Sets the email
+     *
+     * @param email the email to be assigned to an User object
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Sets the user type
+     *
+     * @param userType the user user type to be assigned to an User object
+     */
     public void setUserType(String userType) {
         this.userType = userType;
     }
--- #Thirsty/app/src/main/java/com/example/user/thirsty/userDataBase.java
+++ #Thirsty/app/src/main/java/com/example/user/thirsty/userDataBase.java
@@ -12,7 +12,7 @@ public class userDataBase {
     //private static UserProfile profile;
 
     /**
-     * Constructer for user data base backed by a hashmap
+     * Constructer for user data base backed by a hashmap.
      */
     public userDataBase() {
         userDataBase = new HashMap<String, Object>();
@@ -47,10 +47,10 @@ public class userDataBase {
 
     /**
      * Will return a boolean value confirming whether or not a username already exists within the
-     * database
-     * 
-     * @param username
-     * @return
+     * database.
+     *
+     * @param username the username to search the database for
+     * @return a boolean value indicating whether or not the username exists in the database
      */
     public static boolean getUser(String username) {
         if (userDataBase.containsKey(username)) {
@@ -60,6 +60,16 @@ public class userDataBase {
         }
     }
 
+    /**
+     * Checks the password entered for a particular user agianst the password stored in their user
+     * profile within the user database.
+     *
+     * @param username the username for the user logging in
+     * @param password the password to be checked agaisnt the correct password stored in the data
+     *                 base
+     * @return a boolean value indicating whether of not the password entered with the associated
+     * username was corrrect
+     */
     public static boolean getPassword(String username, String password) {
         if (password.equals(((UserProfile) userDataBase.get(username)).getPassword())) {
             return true;
@@ -68,6 +78,12 @@ public class userDataBase {
         }
     }
 
+    /**
+     * Creates a dynamic toString for the username entered to be displayed upon successful login.
+     *
+     * @param username the username that succefully logged into the system
+     * @return a string containing the email address and user type of the user
+     */
     public static String getInfo(String username) {
         return "Your email is currently set as "
                 + ((UserProfile) userDataBase.get(username)).getEmail()
@@ -76,10 +92,22 @@ public class userDataBase {
 
     }
 
+    /**
+     * Will update the email of a user that already exists within the database.
+     *
+     * @param username the username of the user who is attempting to update their email
+     * @param email the email that is to be updated within the object of that user
+     */
     public static void setEmail(String username, String email) {
         ((UserProfile) userDataBase.get(username)).setEmail(email);
     }
 
+    /**
+     * Will update the password of a user that already exists within the database.
+     *
+     * @param username the username of the user who is attempting to update their password
+     * @param password the password that is to be updated within the object of that user
+     */
     public static void setPassword(String username, String password) {
         ((UserProfile) userDataBase.get(username)).setPassword(password);
     }
