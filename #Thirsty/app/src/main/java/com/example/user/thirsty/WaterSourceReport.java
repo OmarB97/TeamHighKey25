cadb881124511afedb4794dc01006d5acf344c50
Dennis Eddington <Dedding4341@gmail.com>
2017-02-27 14:53:07 -0500

    Small changes

---
+++ #Thirsty/app/src/main/java/com/example/user/thirsty/WaterSourceReport.java
@@ -0,0 +1,20 @@
+package com.example.user.thirsty;
+
+import java.util.Date;
+
+/**
+ * Created by USER on 2/27/2017.
+ */
+
+public class WaterSourceReport {
+    private WaterSourceReport[] backingArray;
+    private int reportNumber;
+    private Date currentDate;
+    private String location;
+    private String waterType;
+    private String waterCondition;
+
+    public WaterSourceReport(String location, String waterType, String waterCondition) {
+        
+    }
+}
